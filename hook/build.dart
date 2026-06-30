3da5ae1a9157e55e2ed9dadfd86b3b91990283db
Vladimir Goychev <vladimir@nicolaudiegroup.com>
2026-06-30 10:13:35 +0300

    fix: exclude mobile from rust build script because it is not supported

--- hook/build.dart
+++ hook/build.dart
@@ -1,8 +1,16 @@
+import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
-void main(List<String> args) async {  
+void main(List<String> args) async {
   await build(args, (input, output) async {
+
+    final targetOS = input.config.code.targetOS;
+
+    if (targetOS == OS.iOS || targetOS == OS.android) {
+      return;
+    }
+
     await const RustBuilder(
       assetName: 'src/lib.rs',
       cratePath: 'rust',
